3d53a6aa4413919d281e38615838ccb5428620c8
dhdhxk <dhdhxk@hotmail.com>
2016-03-18 17:30:56 -0700

    Remove useless component and connect to go to account feature that didn`t applied

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -414,25 +414,6 @@ console.log(test);
     };
 })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
 .controller('AccountCtrl', function($scope, $stateParams, FetchUser, FetchUserPosts, $http, $state, $location) {
     $scope.page = 1;
     $scope.isMyAccount = false;
