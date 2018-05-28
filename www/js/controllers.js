50f8e66efa4db7b03f006f00408ce865119c7940
yu kim <yukim@yus-MacBook-Pro.local>
2018-05-27 19:04:50 -0700

    update invite link

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2780,7 +2780,7 @@ angular.module('starter.controllers', [])
         var options = {
             message: 'which looks better?',
             subject: 'Which Looks Better?',
-            url: $rootScope.baseURL + '/s/demo'
+            url: $rootScope.baseURL + '/s/intro'
         }
         var onSuccess = function(result) {
             console.log("invite succeed");
